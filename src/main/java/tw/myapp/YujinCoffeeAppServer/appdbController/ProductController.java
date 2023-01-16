aa50877a07b66341662488401239c4be3f89488a
Jingyuyuu <rose622612@gmail.com>
2023-01-16 17:40:06 +0800

    想把飲料的資訊抓出來 傳給AndroidStudio 還沒想到怎麼傳

--- src/main/java/tw/myapp/YujinCoffeeAppServer/appdbController/ProductController.java
+++ src/main/java/tw/myapp/YujinCoffeeAppServer/appdbController/ProductController.java
@@ -1,12 +1,28 @@
 package tw.myapp.YujinCoffeeAppServer.appdbController;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tw.myapp.YujinCoffeeAppServer.appdbRepository.productRepository;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
     @Autowired
+    productRepository productRepo;
+    @PostMapping("/register")
+    public String register(@RequestBody String body){
+        JSONObject object=new JSONObject(body);
+        System.out.println("後端接收消息"+body);
+        JSONObject data=object.getJSONObject("regData");
+        System.out.println("name :"+data.getString("name")+"email :"+data.getString("email")
+                +"pwd :"+data.getString("pwd")
+                +"phone :"+data.getString("phone"));
+
+        return productRepo.getProsuct().toString();
+    }
 
 }
--- src/main/java/tw/myapp/YujinCoffeeAppServer/appdbRepository/productRepository.java
+++ src/main/java/tw/myapp/YujinCoffeeAppServer/appdbRepository/productRepository.java
@@ -1,4 +1,18 @@
 package tw.myapp.YujinCoffeeAppServer.appdbRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class productRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public List<Map<String,Object>> getProsuct() {
+        return jdbcTemplate.queryForList("select * from products where series = 1;");
+    }
 }
