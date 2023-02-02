97559f16484cdeebad89ae51f623676e66e0039a
jingyu <rose622612@gmail.com>
2023-02-02 22:38:17 +0800

    0202 22:38

--- src/main/java/tw/myapp/YujinCoffeeAppServer/appdbRepository/storeRepository.java
+++ src/main/java/tw/myapp/YujinCoffeeAppServer/appdbRepository/storeRepository.java
@@ -1,7 +1,20 @@
 package tw.myapp.YujinCoffeeAppServer.appdbRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
-public class storeRepository {
+public class StoreRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public List<Map<String,Object>> getStore() {
+        return jdbcTemplate.queryForList("select * from 分店資料;");
+    }
+
+
 }
